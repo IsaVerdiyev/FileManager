f8f7b374378fea4eb21d321914a661eb3890d265
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2018-03-25 02:26:12 +0400

    Changed default colors of ChoosableButton class in Colors.cpp

--- FileManager/Colors.cpp
+++ FileManager/Colors.cpp
@@ -5,10 +5,10 @@ extern const Color defaultBackground = static_cast<Color>(Black << 4 | White);
 
 extern const Color defaultColor = static_cast<Color>(Brown << 4 | LightGray);
  
-extern const Color defaultButtonColor = static_cast<Color>(Yellow << 4 | White);
+extern const Color defaultButtonColor = static_cast<Color>(Yellow << 4 | Black);
 extern const Color defaultChosenButtonColor = static_cast<Color>(Green << 4 | White);
-extern const Color defaultHoverColor = static_cast<Color>(Magenta << 4 | Cyan);
-extern const Color defaultHoverOnChosenColor = static_cast<Color>(Green << 4 | Cyan);
+extern const Color defaultHoverColor = static_cast<Color>(Brown << 4 | Black);
+extern const Color defaultHoverOnChosenColor = static_cast<Color>(Brown << 4 | White);
 
 extern const Color defaultActiveColor = static_cast<Color>(Red << 4 | Blue);
 extern const Color defaultDeactiveColor = static_cast<Color>(DarkGray << 4 | White);
