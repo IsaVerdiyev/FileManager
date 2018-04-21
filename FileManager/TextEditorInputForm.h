536d2f9bd9428771b4305c668000e158a12e2d68
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2018-04-21 17:18:44 +0400

    Some changes in comments

--- FileManager/TextEditorInputForm.h
+++ FileManager/TextEditorInputForm.h
@@ -3,10 +3,11 @@
 
 /*
 	This class is inherited from InputForm and is used in TextEditor class.
-	It is different from InputForm in only one function : void takeInput(const INPUT_RECORD &event).
+	It is different from InputForm in  function : void takeInput(const INPUT_RECORD &event).
 	Here when trying to insert to end of object  no inserting appears.
 	In InputForm class it is possible to insert at the end of object.
-	Also it is impossible to put cursor on position where there is no symbol added by user
+	It is impossible to put cursor on position where there is no symbol added by user
+	Also there is difference in setting stringSize when is called takeInput function
 */
 
 #include "InputForm.h"
